923d2f09fe779f6ba59d13bfe2920f46771e0567
Michael Williams <michael.williams@example.com>
2025-06-26 00:39:08 -0400

    Added full constructor to OutsourcedPart and updated access modifier for companyName.

--- src/main/java/com/example/demo/domain/OutsourcedPart.java
+++ src/main/java/com/example/demo/domain/OutsourcedPart.java
@@ -11,10 +11,16 @@ import javax.persistence.Entity;
  */
 @Entity
 @DiscriminatorValue("2")
-public class OutsourcedPart extends Part{
-String companyName;
+public class OutsourcedPart extends Part {
+    private String companyName;
 
     public OutsourcedPart() {
+        super();
+    }
+
+    public OutsourcedPart(String name, double price, int inv, int minInv, int maxInv, String companyName) {
+        super(name, price, inv, minInv, maxInv);
+        this.companyName = companyName;
     }
 
     public String getCompanyName() {
