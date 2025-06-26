fbe04bc34cc7d6dddfcbbc6141d9beb80365a2a0
Michael Williams <michael.williams@example.com>
2025-06-26 00:09:27 -0400

    Refactored InhousePart to use superclass fields and constructors. Removed redundant partId property and accessors.

--- src/main/java/com/example/demo/domain/InhousePart.java
+++ src/main/java/com/example/demo/domain/InhousePart.java
@@ -3,25 +3,15 @@ package com.example.demo.domain;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-/**
- *
- *
- *
- *
- */
 @Entity
 @DiscriminatorValue("1")
-public class InhousePart extends Part{
-    int partId;
+public class InhousePart extends Part {
 
     public InhousePart() {
+        super();
     }
 
-    public int getPartId() {
-        return partId;
-    }
-
-    public void setPartId(int partId) {
-        this.partId = partId;
+    public InhousePart(String name, double price, int inv, int minInv, int maxInv) {
+        super(name, price, inv, minInv, maxInv);
     }
 }
